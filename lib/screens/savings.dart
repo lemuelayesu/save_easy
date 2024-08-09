4f5bff08dde71a539cea49138ac402e9e0573915
lemuelayesu <leninnation1@gmail.com>
2024-08-09 05:04:57 +0000

    august 09 update

--- lib/screens/savings.dart
+++ lib/screens/savings.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: dead_code
+
 import 'package:flutter/material.dart';
 
 class Savings extends StatefulWidget {
@@ -14,7 +16,9 @@ class _SavingsState extends State<Savings> {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-          onPressed: null,
+          onPressed: () {
+            Navigator.pop(context);
+          },
           icon: Icon(
             Icons.arrow_back_ios,
             color: color.onSurface,
@@ -33,20 +37,201 @@ class _SavingsState extends State<Savings> {
         ],
         centerTitle: true,
         title: Text(
-          "Savings Goals",
+          "Saving Goals",
           style: TextStyle(
             fontSize: 20,
-            fontWeight: FontWeight.w500,
+            fontWeight: FontWeight.w600,
             color: color.onSurface,
           ),
         ),
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+        padding: const EdgeInsets.symmetric(horizontal: 13, vertical: 10),
         child: ListView(
-          children: [],
+          children: [
+            SavingGoalCard(
+              cardColor: color.secondary,
+              itemLabel: 'Iphone 13 Mini',
+              savingsProgressIndicator: '\$300 of \$699',
+              percentageProgressIndicator: 0.5,
+              daysLeft: "14 days left",
+              cardTextColor: color.surface,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            SavingGoalCard(
+              cardColor: color.secondaryFixed,
+              itemLabel: "Macbook Pro M1",
+              savingsProgressIndicator: "\$300 of \$1,499",
+              percentageProgressIndicator: 0.3,
+              daysLeft: "14 days left",
+              cardTextColor: color.onSurface,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            SavingGoalCard(
+              cardColor: color.primaryFixed,
+              itemLabel: 'School Fees',
+              savingsProgressIndicator: '\$10,000 of \$20,000',
+              percentageProgressIndicator: 0.9,
+              daysLeft: "30 days left",
+              cardTextColor: color.onSurface,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            SavingGoalCard(
+              cardColor: color.primary,
+              itemLabel: 'Capital',
+              savingsProgressIndicator: '\$65,000 of \$30,500',
+              percentageProgressIndicator: 0.9,
+              daysLeft: "60 days left",
+              cardTextColor: color.onSurface,
+            ),
+          ],
         ),
       ),
+    );
+  }
+}
+
+class SavingGoalCard extends StatelessWidget {
+  const SavingGoalCard({
+    super.key,
+    required this.cardColor,
+    required this.itemLabel,
+    required this.savingsProgressIndicator,
+    required this.percentageProgressIndicator,
+    required this.daysLeft,
+    required this.cardTextColor,
+  });
+
+  final Color cardColor;
+  final String itemLabel;
+  final String savingsProgressIndicator;
+  final double percentageProgressIndicator;
+  final String daysLeft;
+  final Color cardTextColor;
+
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme color = Theme.of(context).colorScheme;
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 13, vertical: 16),
+          height: 176,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: cardColor,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Stack(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    itemLabel,
+                    style: TextStyle(
+                        fontSize: 18,
+                        color: color.surface,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Balance",
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w200,
+                          color: color.surface,
+                        ),
+                      ),
+                      Text(
+                        "${(percentageProgressIndicator * 100).toStringAsFixed(0)}%",
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w200,
+                          color: cardTextColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  LinearProgressIndicator(
+                    value: percentageProgressIndicator,
+                    backgroundColor: color.surface.withOpacity(0.7),
+                    minHeight: 5,
+                    color: color.onSurface,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        savingsProgressIndicator,
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w200,
+                          color: cardTextColor,
+                        ),
+                      ),
+                      Text(
+                        daysLeft,
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w200,
+                          color: cardTextColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 7,
+                  ),
+                  Container(
+                    margin: const EdgeInsetsDirectional.only(end: 30),
+                    alignment: Alignment.bottomRight,
+                    child: GestureDetector(
+                      child: CircleAvatar(
+                        backgroundColor: color.surface,
+                        radius: 16,
+                        child: const Center(
+                          child: Icon(
+                            Icons.add,
+                            size: 20,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Align(
+                alignment: Alignment.topRight,
+                child: Icon(
+                  Icons.assured_workload_rounded,
+                  color: color.onSurface.withOpacity(0.15),
+                  size: 120,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
